a5b72f1463ccbe772966392ed100d4585ac85ea8
claws425 <73760859+claws425@users.noreply.github.com>
2021-04-10 14:37:05 -0400

    added new folder and started app code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,24 @@
 // @TODO: YOUR CODE HERE!
+var width = parseInt(d3.select("#scatter").style("width"));
+
+var height = width - width / 4;
+
+var margin = 15;
+
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
+  .attr("class", "chart");
+
+  var circRadius;
+function crGet() {
+  if (width <= 530) {
+    circRadius = 5;
+  }
+  else {
+    circRadius = 10;
+  }
+}
+crGet();
