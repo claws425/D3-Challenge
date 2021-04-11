1bdf579571922161083d9ac85f2b2ad3b2122f11
claws425 <73760859+claws425@users.noreply.github.com>
2021-04-11 16:13:04 -0400

    set margins for the graph

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,8 @@ var width = parseInt(d3.select("#scatter").style("width"));
 // Designate the height of the graph
 var height = width - width / 4;
 
+// Set margins all around
+ var margin = 20;
+
+ 
+
